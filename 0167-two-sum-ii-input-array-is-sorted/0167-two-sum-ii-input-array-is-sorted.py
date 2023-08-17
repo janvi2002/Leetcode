3d81912f53bda64456f636f01be6331a32d459ed
Janvi gurjar <78970696+janvi2002@users.noreply.github.com>
2023-08-18 01:41:07 +0530

    Time: 114 ms (93.97%), Space: 17.1 MB (93.12%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        dic={}
+        for i in range(len(numbers)):
+            if target-numbers[i] not in dic:
+                dic[numbers[i]]=i
+            else:
+                return [dic[target-numbers[i]]+1,i+1]
