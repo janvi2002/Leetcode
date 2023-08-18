bb77f7dd4b461396354ec3fa9fc07a1e20ebbcc1
Janvi gurjar <78970696+janvi2002@users.noreply.github.com>
2023-08-18 16:42:26 +0530

    Time: 104 ms (58.86%), Space: 17 MB (30.85%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.py
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.py
@@ -1,11 +1,29 @@
 class Solution:
     def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
-#         brute force solution
-        n=len(intervals)
-        c=0
-        for i in range(n):
-            for j in range(n):
-                if i!=j and intervals[j][0] <= intervals[i][0] and intervals[i][1] <= intervals[j][1]:
-                    c+=1
-                    break
-        return n-c
+        intervals.sort(key=lambda x:(x[0],-x[1]))
+        l=-1
+        r=-1
+        res=0
+        for i in intervals:
+            if i[0]>=l and i[1]<=r:
+                continue
+            res+=1
+            l=i[0]
+            r=i[1]
+        return res
+#         brute force solution O(n^2)
+        # n=len(intervals)
+        # c=0
+        # for i in range(n):
+        #     for j in range(n):
+        #         if i!=j and intervals[j][0] <= intervals[i][0] and intervals[i][1] <= intervals[j][1]:
+        #             c+=1
+        #             break
+        # return n-c
+#     otpimizing using sorting O(nlogn)
+
+    
+    
+
+    
+    
