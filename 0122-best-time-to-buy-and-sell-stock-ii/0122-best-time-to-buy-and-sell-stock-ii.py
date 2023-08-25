1035b15e53a081689c08926b98cc82ca45510ca0
Janvi gurjar <78970696+janvi2002@users.noreply.github.com>
2023-08-25 14:39:16 +0530

    Time: 64 ms (78.52%), Space: 17.7 MB (60.03%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -2,6 +2,7 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         ans=0
         for i in range(1,len(prices)):
+#             simply check if ith is greter than i-1 then we can by or sell it and add simple profit to the answer
             if prices[i-1]<prices[i]:
                 ans+=prices[i]-prices[i-1]
         return ans
