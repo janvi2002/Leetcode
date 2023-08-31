a61d48aad3db149c3d7e0ebb4d67a90cd4b1cf12
Janvi gurjar <78970696+janvi2002@users.noreply.github.com>
2023-09-01 01:10:23 +0530

    Time: 67 ms (58.19%), Space: 16.7 MB (27.79%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        dic={}
+        for i in magazine:
+            dic[i]=dic.get(i,0)+1
+        
+        for i in ransomNote:
+            if i in dic and dic[i]>0:
+                dic[i]-=1
+            else:
+                return False
+        return True
