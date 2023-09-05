d911cf5f72f7a745d453eb00abf8cb2b3e320ba5
Janvi gurjar <78970696+janvi2002@users.noreply.github.com>
2023-09-05 15:19:30 +0530

    Time: 44 ms (83.77%), Space: 18.9 MB (53.13%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -7,14 +7,17 @@ class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:
             return None
+#         for odd even indexes 
         odd=head
         even=head.next
+#     for not lossing even index pointer
         even_head=even
         while even and even.next:
+#      here we pointing odd index to odd then move the pointer 
             odd.next=odd.next.next
             odd=odd.next
             even.next=even.next.next
             even=even.next
-
+#    at last odd.next is poinitng to even starting point
         odd.next=even_head
         return head
