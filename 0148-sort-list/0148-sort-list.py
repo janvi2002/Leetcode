ab221b7ee01c654dc6420d335e4b4aaa0c6c81cf
Janvi gurjar <78970696+janvi2002@users.noreply.github.com>
2023-09-06 20:29:56 +0530

    Time: 477 ms (60.36%), Space: 38.6 MB (97.85%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -5,19 +5,54 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        arr=[]
-        ptr=head
-        while ptr:
-            arr.append(ptr.val)
-            ptr=ptr.next
-        arr.sort()
-        dummy=ListNode(0)
-        ptr=dummy
-        for i in arr:
-            new=ListNode(i)
-            ptr.next=new
+        if not head or not head.next:
+            return head
+#         finding middle
+        slow=head
+        fast=head.next
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+#         divide from middle
+        mid=slow.next
+        slow.next=None
+        left=self.sortList(head)
+        right=self.sortList(mid)
+        
+        ans= ListNode(0)
+        ptr=ans
+        if not left:
+            return right
+        if not right:
+            return left
+        
+        while left and right:
+            if left.val < right.val :
+                ptr.next = left
+                left=left.next
+            else:
+                ptr.next=right
+                right=right.next
             ptr=ptr.next
         
-        return dummy.next
-            
+        if left:
+            ptr.next=left
+        elif right:
+            ptr.next=right
+        return ans.next
+     
         
+#         arr=[]
+#         ptr=head
+#         while ptr:
+#             arr.append(ptr.val)
+#             ptr=ptr.next
+#         arr.sort()
+#         dummy=ListNode(0)
+#         ptr=dummy
+#         for i in arr:
+#             new=ListNode(i)
+#             ptr.next=new
+#             ptr=ptr.next
+        
+#         return dummy.next
