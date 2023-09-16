916728d0aea27dd5eb041a3412c9c567c6d58b01
Janvi gurjar <78970696+janvi2002@users.noreply.github.com>
2023-09-16 18:22:16 +0530

    Time: 48 ms (6.56%), Space: 16.3 MB (92.08%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div225-implement-stack-using-queues/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div225-implement-stack-using-queues.py
@@ -0,0 +1,33 @@
+from queue import Queue
+class MyStack:
+
+    def __init__(self):
+        self.q = Queue()
+
+    def push(self, x: int) -> None:
+        size = self.q.qsize()
+        self.q.put(x)
+        
+        for i in range(size):
+            self.q.put(self.q.get())
+
+    def pop(self) -> int:
+        if self.empty():
+            return False
+        return self.q.get()
+
+    def top(self) -> int:
+        if self.empty():
+            return False
+        return self.q.queue[0]
+
+    def empty(self) -> bool:
+        return self.q.empty()
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
